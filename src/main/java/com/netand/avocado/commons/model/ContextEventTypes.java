11647d86514c04ec2db900c8fb505e06dc94b0a3
joshua <minjae@netand.co.kr>
2021-05-17 12:22:53 +0900

    Revert "."
    This reverts commit 96928fce75d8d289243522c415f1b43f5a42f11f.
    
    # Conflicts:
    #	.gradle/6.5/executionHistory/executionHistory.bin
    #	.gradle/6.5/executionHistory/executionHistory.lock
    #	.gradle/6.5/fileHashes/fileHashes.bin
    #	.gradle/6.5/fileHashes/fileHashes.lock
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.gradle/buildOutputCleanup/outputFiles.bin
    #	.idea/workspace.xml

---
+++ src/main/java/com/netand/avocado/commons/model/ContextEventTypes.java
@@ -0,0 +1,49 @@
+package com.netand.avocado.commons.model;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Getter
+public enum ContextEventTypes {
+
+	//@formatter:off
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	Writing     ( 10 ),
+	Submit      ( 11 ),
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	Approving   ( 20 ),
+	Approved    ( 21 ),
+	Rejected    ( 22 ),
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	Created     ( 30 ),
+	Updated     ( 31 ),
+	Deleted     ( 32 ),
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	Granted     ( 40 ),
+	Revoked     ( 41 ),
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//@formatter:on
+	;
+
+	private static final Map<Integer, ContextEventTypes> INDEX_FOR_CODE = new LinkedHashMap<>();
+	private final int code;
+
+	static {
+
+		Arrays.stream( ContextEventTypes.values() ).forEach( item -> INDEX_FOR_CODE.put( item.getCode(), item ) );
+	}
+
+	ContextEventTypes( int code ) {
+
+		this.code = code;
+	}
+
+	public static Optional< ContextEventTypes > findByCode( int code ) {
+
+		return Optional.ofNullable( INDEX_FOR_CODE.get( code ) );
+	}
+}
