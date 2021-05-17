7b3d82c4f87f1ee1e3ee950fe3e7470e1ea05a69
rim <sylph216@hanmail.net>
2021-05-17 16:02:42 +0900

    1. Consts : JSON_OBJECT_MAPPER 추가 2. ApplicationCodes 추가

---
+++ src/main/java/com/netand/avocado/commons/enums/ApplicationCodes.java
@@ -0,0 +1,49 @@
+package com.netand.avocado.commons.enums;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Getter
+public enum ApplicationCodes {
+
+	//@formatter:off
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	OPEN_API        ( "open-api",           "Open API Server" ),
+	IAM             ( "iam",                "Identity Access Management" ),
+	PAM             ( "pam",                "Privileged Access Management" ),
+	ADMIN           ( "admin",              "Administrator Server" ),
+	BASTION         ( "bastion",            "Bastion" ),
+	;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//@formatter:on
+
+	private static final Map< String, ApplicationCodes > INDEX_FOR_CODE = new LinkedHashMap<>();
+
+	static {
+
+		Arrays.stream( ApplicationCodes.values() ).forEach( item -> {
+
+					INDEX_FOR_CODE.put( item.code, item );
+				}
+		);
+	}
+
+	private String code;
+	private String description;
+
+	ApplicationCodes( String code, String description ) {
+
+		this.code = code;
+		this.description = description;
+	}
+
+	public static Optional< ApplicationCodes > findByCode( String code ) {
+
+		return Optional.ofNullable( INDEX_FOR_CODE.get( code ) );
+	}
+}
