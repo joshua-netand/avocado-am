edb8e8c3b7e0077af73c347a3c6c9752ebb6f099
joshua <minjae@netand.co.kr>
2021-06-30 11:45:25 +0900

    Commons   - JsonArrayConverter PAM   - 사용자/그룹 속성기반 권한 데이터핸들러 및 DTO 모델   - 엔트리 모델 수정

---
+++ src/main/java/com/netand/avocado/commons/model/converter/jpa/JsonArrayConverter.java
@@ -0,0 +1,57 @@
+package com.netand.avocado.commons.model.converter.jpa;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import javax.persistence.AttributeConverter;
+import java.util.Arrays;
+import java.util.List;
+
+public class JsonArrayConverter implements AttributeConverter< List< String >, String > {
+
+	private static final ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json()
+			.featuresToDisable( SerializationFeature.FAIL_ON_EMPTY_BEANS )
+			.featuresToDisable( SerializationFeature.WRITE_DATES_AS_TIMESTAMPS )
+			.modules( new JavaTimeModule() )
+			.build();
+
+	@Override
+	public String convertToDatabaseColumn( List< String > list ) {
+
+		if ( list == null || list.isEmpty()) {
+
+			return null;
+		}
+
+		try {
+
+			return objectMapper.writeValueAsString( list );
+
+		} catch ( JsonProcessingException e ) {
+
+			throw new RuntimeException( e );
+		}
+	}
+
+	@Override
+	public List< String > convertToEntityAttribute( String jsonArrayString ) {
+
+		if ( StringUtils.isBlank(jsonArrayString)) {
+
+			return null;
+		}
+
+		try {
+
+			return Arrays.asList( objectMapper.readValue( jsonArrayString, String[].class ) );
+
+		} catch ( JsonProcessingException e ) {
+
+			throw new RuntimeException( e );
+		}
+	}
+}
